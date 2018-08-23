8ba567a5d1bfdef1d93227eb249ffca74a08d377
Nathaniel Rubin <nateboop@gmail.com>
2018-08-22 20:16:45 -0400

    Add marking js package and modal

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,39 @@
+function renderModal(paragraphText) {
+  $("body").prepend("<div class='cpe_overlay'> \
+    <div class='cpe_container'> \
+      <h3 class='cpe_modal_title'>SELECTED TEXT</h3> \
+      <p class='cpe_modal_body'>"+
+    paragraphText
+    +"</p> \
+      <h3 class='cpe_modal_title'>YOUR COMMENT</h3> \
+      <form> \
+        <textarea class='cpe_modal_textarea' placeholder='What did you think of this section?'></textarea> \
+        <input type='submit' value='SUBMIT' class='cpe_modal_submission'/> \
+      </form> \
+    </div> \
+  </div>");
+
+  $(document).on("click", function(event) {
+    if (event.target.className === 'cpe_overlay') {
+      $(".cpe_overlay").remove();
+    }
+  });
+}
+
 $(document).ready(function() {
+  var instance = new Mark(document.querySelector("body"));
+
   $(document).bind('keypress', function(event) {
     if (event.which === 11 ) {
-      var range = window.getSelection().getRangeAt(0),
-        span = document.createElement('blah');
-      $(span).css("background-color", "red")
-      span.appendChild(range.extractContents());
-      range.insertNode(span);
+      var highlightedText = window.getSelection().toString();
+      var instance = new Mark(document.querySelector("body"));
+
+      $("body").mark(highlightedText, {
+        separateWordSearch: false,
+        acrossElements: true,
+        className: "cpe_highlight"
+      });
+      renderModal(highlightedText)
     }
   });
 });
