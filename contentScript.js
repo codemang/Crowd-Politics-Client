d0560cf01d8c7efe97ba93546b1391f97e9c47aa
Nathaniel Rubin <nateboop@gmail.com>
2018-09-17 22:23:20 -0400

    working UX of side panel with no real comments

--- contentScript.js
+++ contentScript.js
@@ -1,133 +1,165 @@
-var globalHighlights = {}
+window.globalHighlights = {};
+window.nate = 'This Worked!';
 
 function renderModal(highlightedObj) {
-  var highlightedText = highlightedObj.highlightedText || globalHighlights[highlightedObj.highlightId].body;
-  $("body").prepend("<div class='cpe_overlay'> \
+  const highlightedText =
+    highlightedObj.highlightedText ||
+    globalHighlights[highlightedObj.highlightId].body;
+  $('body').prepend(
+    "<div class='cpe_overlay'> \
     <div class='cpe_container'> \
       <h3 class='cpe_modal_title'>SELECTED TEXT</h3> \
-      <p class='cpe_modal_body'>"+
-    highlightedText
-    +"</p> \
+      <p class='cpe_modal_body'>" +
+      highlightedText +
+      "</p> \
       <h3 class='cpe_modal_title'>YOUR COMMENT</h3> \
       <form id='cpe_form'> \
         <textarea class='cpe_modal_textarea' placeholder='What did you think of this section?'></textarea> \
         <input type='submit' value='SUBMIT' class='cpe_modal_submission'/> \
       </form> \
     </div> \
-  </div>");
+  </div>",
+  );
 
-  $(document).on("click", function(event) {
+  $(document).on('click', event => {
     if (event.target.className === 'cpe_overlay') {
-      $(".cpe_overlay").remove();
+      $('.cpe_overlay').remove();
     }
   });
 
-  $("#cpe_form").on("submit", function(e) {
+  $('#cpe_form').on('submit', e => {
     e.preventDefault();
 
-    var data = {
-      highlightedText: highlightedText,
+    const data = {
+      highlightedText,
       articleTitle: getArticleTitle(),
       highlightedId: highlightedObj.highlightId,
       url: window.location.host + window.location.pathname,
-      comment: $(".cpe_modal_textarea")[0].value,
+      comment: $('.cpe_modal_textarea')[0].value,
       type: 'post_highlight',
-    }
+    };
 
-    chrome.runtime.sendMessage(data, function(highlight) {
+    chrome.runtime.sendMessage(data, highlight => {
       // TODO: Where is this response key coming from?
       highlight = highlight.response;
       globalHighlights[highlight.id] = highlight;
       highlightText(highlightedText, highlight.id);
-      $(".cpe_overlay").remove();
+      $('.cpe_overlay').remove();
     });
-  })
+  });
 }
 
 function getArticleTitle() {
   if (window.location.host.indexOf('nationalreview') !== -1) {
-    return $("h1.article-header__title").text().trim();
-  } else if (window.location.host.indexOf('nytimes') !== -1) {
-    return $("span.balancedHeadline").text().trim();
-  } else if (window.location.host.indexOf('vox') !== -1) {
-    return $("h1.c-page-title").text().trim();
-  } else if (window.location.host.indexOf('thefederalist') !== -1) {
-    return $("h2.entry-title a").text().trim();
-  } else if (window.location.host.indexOf('salon') !== -1) {
-    return $(".title-container h1").text().trim();
-  } else if (window.location.host.indexOf('washingtonpost') !== -1) {
-    return $(".topper-headline h1").text().trim();
+    return $('h1.article-header__title')
+      .text()
+      .trim();
+  }
+  if (window.location.host.indexOf('nytimes') !== -1) {
+    return $('span.balancedHeadline')
+      .text()
+      .trim();
+  }
+  if (window.location.host.indexOf('vox') !== -1) {
+    return $('h1.c-page-title')
+      .text()
+      .trim();
+  }
+  if (window.location.host.indexOf('thefederalist') !== -1) {
+    return $('h2.entry-title a')
+      .text()
+      .trim();
+  }
+  if (window.location.host.indexOf('salon') !== -1) {
+    return $('.title-container h1')
+      .text()
+      .trim();
+  }
+  if (window.location.host.indexOf('washingtonpost') !== -1) {
+    return $('.topper-headline h1')
+      .text()
+      .trim();
   }
 }
 
 function highlightText(text, highlightId) {
-  var instance = new Mark(document.querySelector("body"));
-  var highlightClass = 'cpe_highlight_' + highlightId;
+  const instance = new Mark(document.querySelector('body'));
+  const highlightClass = 'cpe_highlight_' + highlightId;
 
-  $("body").mark(text, {
+  $('body').mark(text, {
     separateWordSearch: false,
     acrossElements: true,
-    className: "cpe_highlight " + highlightClass,
-    done: function() {
-      $("."+highlightClass).on("click", function(elm) {
-        showComments(highlightId)
+    className: 'cpe_highlight ' + highlightClass,
+    done() {
+      $('.' + highlightClass).on('click', elm => {
+        showComments(highlightId);
       });
-    }
+    },
   });
 }
 
 function showComments(highlightId) {
-  var commentsHtml = ""
-  $.each(globalHighlights[highlightId].comments, function(index, elm) {
-    commentsHtml += "<div class='cpe_comment_container'> \
-      <div class='cpe_comment_username'>" + elm.username + "</div> \
-      <div class='cpe_comment_body'>"+elm.comment + "</div></div>";
+  let commentsHtml = '';
+  $.each(globalHighlights[highlightId].comments, (index, elm) => {
+    commentsHtml +=
+      "<div class='cpe_comment_container'> \
+      <div class='cpe_comment_username'>" +
+      elm.username +
+      "</div> \
+      <div class='cpe_comment_body'>" +
+      elm.comment +
+      '</div></div>';
   });
 
-  $("body").prepend("<div class='cpe_overlay'> \
+  $('body').prepend(
+    "<div class='cpe_overlay'> \
     <div class='cpe_container'> \
       <h3 class='cpe_modal_title'>SELECTED TEXT</h3> \
-      <p class='cpe_modal_body'>"+
-    globalHighlights[highlightId].body
-    +"</p> \
+      <p class='cpe_modal_body'>" +
+      globalHighlights[highlightId].body +
+      "</p> \
       <h3 class='cpe_modal_title'>COMMENTS</h3>" +
-      commentsHtml
-    +"<input type='submit' value='ADD COMMENT' class='cpe_modal_submission' id='add_comment_button'/> \
+      commentsHtml +
+      "<input type='submit' value='ADD COMMENT' class='cpe_modal_submission' id='add_comment_button'/> \
     </div> \
-  </div>");
+  </div>",
+  );
 
-  $(".cpe_comment_container").linkify({ target: "_blank" });
+  $('.cpe_comment_container').linkify({ target: '_blank' });
 
-  $(document).on("click", function(event) {
+  $(document).on('click', event => {
     if (event.target.className === 'cpe_overlay') {
-      $(".cpe_overlay").remove();
+      $('.cpe_overlay').remove();
     }
   });
 
-  $("#add_comment_button").on('click', function() {
-    $(".cpe_overlay").remove();
-    renderModal({highlightId: highlightId})
-  })
+  $('#add_comment_button').on('click', () => {
+    $('.cpe_overlay').remove();
+    renderModal({ highlightId });
+  });
 }
 
-$(document).ready(function() {
-  var message = {
-    type: 'load_highlights',
-    url: window.location.host + window.location.pathname
-  }
-  chrome.runtime.sendMessage(message, function(response) {
-    if (response.response.highlights) {
-      response.response.highlights.forEach(function(highlight) {
-        globalHighlights[highlight.id] = highlight;
-        highlightText(highlight.body, highlight.id)
-      })
-    }
-  });
-
-  $(document).bind('keypress', function(event) {
-    if (event.which === 11 ) {
-      var highlightedText = window.getSelection(0).toString();
-      renderModal({highlightedText: highlightedText})
-    }
-  });
+$(document).ready(() => {
+  // var message = {
+  //   type: 'load_highlights',
+  //   url: window.location.host + window.location.pathname
+  // }
+  // chrome.runtime.sendMessage(message, function(response) {
+  //   if (response.response.highlights) {
+  //     response.response.highlights.forEach(function(highlight) {
+  //       window.globalHighlights[highlight.id] = highlight;
+  //       highlightText(highlight.body, highlight.id)
+  //     })
+  //   }
+  // });
+  //
+  // $(document).bind('keypress', function(event) {
+  //   if (event.which === 11 ) {
+  //     var highlightedText = window.getSelection(0).toString();
+  //     renderModal({highlightedText: highlightedText})
+  //   }
+  // });
 });
+
+console.log('Adding');
+$('body').prepend("<div id='cpe-app'></div>");
